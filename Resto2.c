c753e0078bbce9910c8cd581689d12315469feb0
Julio-Tischer <juliotischer@alunos.utfpr.edu.br>
2025-04-09 10:32:58 -0300

    Exercicio 2

---
+++ Resto2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<locale.h>
+
+int main(){
+    int n,erro,i;
+    printf("Escolha um numero inteiro\t");
+    erro = scanf("%d",&n);
+    i=1;
+
+    if (erro == 0){
+        printf("\n\nESCOLHA UM NUMERO INTEIRO");
+        return 1;
+    }
+
+    while (i<=1100){
+        if (i%n==0){
+            printf("%d\n",i);
+        }
+        i++;
+    }
+    printf("esses numeros são todos os numeros divisiveis por %d entre 1 a 100",n);
+    return 0;
+}

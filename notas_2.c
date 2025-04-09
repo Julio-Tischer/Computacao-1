646489d6f3c12f99c8ebe135238f1e2c2e3e0d5c
Julio-Tischer <juliotischer@alunos.utfpr.edu.br>
2025-04-09 08:15:18 -0300

    Update notas_2.c
    - Frequencia foi alterada de Float para Int

--- notas_2.c
+++ notas_2.c
@@ -4,31 +4,32 @@
 int main(){
     setlocale(LC_ALL,"");
 
-    float Nota1, Nota2, Nota3, Media, Frequencia;
+    float Nota1, Nota2, Nota3, Media;
+    int Frequencia;
     printf("Seja bem-vindo, para saber seu status de aprovação, por favor digite suas notas (com espaço entre cada uma)\t");
     scanf("%f %f %f",&Nota1,&Nota2,&Nota3);
     Media = (Nota1+Nota2+Nota3)/3.0;
     //Primeiro definimos nossas variaveis todas como float, então pegamos as notas e calculamos a media
 
     printf("\n\n Por favor insira a sua frequencia \t");
-    scanf("%f",&Frequencia);
+    scanf("%d",&Frequencia);
     //Isto pega a frequencia
 
     if (Frequencia>= 75 && Media>8.0){
 
-        printf("\n\n Parabens! Você foi aprovado com DISTINÇÃO. Sua media é %.1f e sua frequencia foi %.1f%%",Media,Frequencia);
+        printf("\n\n Parabens! Você foi aprovado com DISTINÇÃO. Sua media é %.1f e sua frequencia foi %d%%",Media,Frequencia);
     }
     else if (Media>=6 && Frequencia >= 75){
 
-        printf("\n\n Parabens, você esta aprovado. Sua media é %.1f e sua frequencia foi %.1f%%",Media,Frequencia);
+        printf("\n\n Parabens, você esta aprovado. Sua media é %.1f e sua frequencia foi %d%%",Media,Frequencia);
     }
     else if ((Media>=4 && Media<=6 && Frequencia>=75 && Frequencia>50)||(Media>=6 && Frequencia<75 && Frequencia>50)||(Media>=4 && Frequencia<75 && Frequencia>50)){
 
-        printf("\n\n Melhor se esforçar, você esta em exame com media %.1f e frequencia %.1f%%",Media,Frequencia);
+        printf("\n\n Melhor se esforçar, você esta em exame com media %.1f e frequencia %d%%",Media,Frequencia);
     }
     else if (Media<4 || Frequencia<=50){
 
-        printf("\n\n Você esta reprovado, pois sua nota e/ou frequencia foram muito baixas. Sua media foi de %.1f e frequencia foi de %.1f%%",Media,Frequencia);
+        printf("\n\n Você esta reprovado, pois sua nota e/ou frequencia foram muito baixas. Sua media foi de %.1f e frequencia foi de %d%%",Media,Frequencia);
     }
     else{
 

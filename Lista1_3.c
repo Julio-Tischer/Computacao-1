c98fe64981e52a555dfce1e75e87df3591370d07
Julio-Tischer <juliotischer@alunos.utfpr.edu.br>
2025-04-16 07:59:14 -0300

    Criado codigo

---
+++ Lista1_3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<locale.h>
+int main() {
+    setlocale(LC_ALL,"");
+    int casos,i = 1;
+    float media1,media2,media3;
+
+    printf("Quantos casos de média vai calcular? \t");
+    int erro=scanf("%d",&casos);
+        if (erro == 0){
+            printf("\n\n!!!!!!\n!Erro!\n!!!!!!\n\n");
+            return 1;
+        }
+
+    while (i<=casos){
+        printf("\n\nDigite os numeros do %d° de %d casos a serem calculados:\t",i,casos);
+        scanf("%f %f %f",&media1,&media2,&media3);
+        printf("\n\n A média é %.2f",((media1*2)+(media2*3)+(media3*5))/10);
+
+        i++;
+
+    }
+    printf("\n\nTodas as %d médias foram calculadas com sucesso!\n",casos);
+
+    return 0;
+
+}

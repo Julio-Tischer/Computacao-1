516aa1ed65b3622e6a79aaaa38375e4393d8c643
Julio-Tischer <juliotischer@alunos.utfpr.edu.br>
2025-05-21 10:15:01 -0300

    Update matriz_ex2.c

--- matriz_ex2.c
+++ matriz_ex2.c
@@ -33,7 +33,8 @@ void mtrxPrint(int matrix[N][N]){
     int i,j;
     for (i=0;i<N;i++){
         for (j=0;j<N;j++){
-            printf("%d  ",matrix[i][j]);
+            if (matrix[i][j]!=0){printf("%d  ",matrix[i][j]);}
+            else {printf(" ");}
         }
         printf("\n");
     }

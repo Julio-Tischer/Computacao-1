d75ad05833232fc804e057257675b35c95a3ccf0
Julio-Tischer <juliotischer@alunos.utfpr.edu.br>
2025-05-21 10:07:02 -0300

    Esse tambem fico brabo

---
+++ matriz_ex2.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#define N 5
+
+void zeroMatrix(int [N][N]);
+void mtrxPrint(int [N][N]);
+
+int main(){
+
+    int i,j;
+    int pascal[N][N];
+    zeroMatrix(pascal);
+
+    for(i=0;i<N;i++){
+        for (j=1;j<i;j++){
+            pascal[i][j]=pascal[i-1][j-1]+pascal[i-1][j];
+        }
+    }
+
+    mtrxPrint(pascal);
+}
+
+void zeroMatrix(int matrix [N][N]){
+    int i,j;
+    for (i=0;i<N;i++){
+        for (j=0;j<N;j++){
+            if(i==j||j==0)matrix[i][j]=1;
+            else{matrix[i][j]=0;}
+        }
+    }
+}
+
+void mtrxPrint(int matrix[N][N]){
+    int i,j;
+    for (i=0;i<N;i++){
+        for (j=0;j<N;j++){
+            printf("%d  ",matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
